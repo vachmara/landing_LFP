45bd072945c371781ef76429450f91f1c0c38d47
Valentin Chmara <valentinchmara@gmail.com>
2024-01-02 16:23:59 +0100

    feat: add legal page

--- pages/legal.vue
+++ pages/legal.vue
@@ -1,6 +1,107 @@
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 
 <template>
-  <div></div>
+  <div class="font-sans">
+    <Navbar />
+
+    <div class="container mx-auto min-h-[80vh] py-8 px-4">
+      <h1 class="text-2xl font-bold mb-6">Mentions légales</h1>
+
+      <p class="mb-4"><strong>La Famiglia SAS</strong></p>
+      <p class="mb-4">
+        73 avenue du général de Gaulle<br />94170 Le Perreux Sur Marne
+      </p>
+      <p class="mb-4">SIRET : 977 552 793<br />R.C.S. Créteil</p>
+      <p class="mb-4">Directeur de la publication : Elie Zemmour</p>
+      <p class="mb-4">
+        Hébergeur : Vercel Inc., située 340 S Lemon Ave #4133 Walnut, CA 91789,
+        et joignable au (559) 288-7060.
+      </p>
+      <p class="mb-4">
+        Contact :
+        <a href="mailto:lafamiglia-contact@gmail.com" class="text-blue-600">
+          lafamiglia-contact@gmail.com</a
+        >
+      </p>
+
+      <h2 class="text-xl font-semibold my-6">Conditions d'utilisation</h2>
+      <p class="mb-4">
+        Bienvenue sur le site web de La Famiglia SAS. En utilisant notre site et
+        nos services de réservation en ligne, vous acceptez les conditions
+        d'utilisation suivantes. Veuillez les lire attentivement.
+      </p>
+
+      <h3 class="text-lg font-semibold my-4">1. Réservation de rendez-vous</h3>
+      <p class="mb-4">
+        1.1. En utilisant notre service de réservation en ligne via la
+        plateforme Calendly, vous consentez également aux conditions
+        d'utilisation de Calendly.
+      </p>
+      <p class="mb-4">
+        1.2. La confirmation d'un rendez-vous est sujette à la disponibilité de
+        nos services, et La Famiglia SAS se réserve le droit d'ajuster ou
+        d'annuler un rendez-vous en fonction de la disponibilité.
+      </p>
+
+      <h3 class="text-lg font-semibold my-4">
+        2. Services de levée de fonds et de croissance
+      </h3>
+      <p class="mb-4">
+        2.1. La Famiglia SAS propose des services d'accompagnement pour la levée
+        de fonds et la croissance via le marketing digital. Les détails
+        spécifiques de ces services seront discutés lors des rendez-vous
+        réservés qui seront ensuite contractualisés et mis sur un devis
+        détaillé.
+      </p>
+
+      <h3 class="text-lg font-semibold my-4">3. Responsabilités</h3>
+      <p class="mb-4">
+        3.1. La Famiglia SAS ne peut être tenue responsable des résultats
+        spécifiques découlant des services fournis, car ils dépendent de divers
+        facteurs.
+      </p>
+      <p class="mb-4">
+        3.2. Les utilisateurs sont responsables de la confidentialité de leurs
+        informations de réservation et des informations partagées lors des
+        rendez-vous.
+      </p>
+
+      <h3 class="text-lg font-semibold my-4">4. Utilisation de Calendly</h3>
+      <p class="mb-4">
+        4.1. Les utilisateurs réservent des rendez-vous via la plateforme
+        Calendly. En utilisant Calendly, vous acceptez également les conditions
+        d'utilisation de Calendly. Pour en savoir plus, veuillez consulter
+        <a
+          href="https://calendly.com/terms"
+          class="text-blue-600"
+          target="_blank"
+          rel="noopener noreferrer"
+          >https://calendly.com/terms</a
+        >.
+      </p>
+
+      <h3 class="text-lg font-semibold my-4">
+        5. Modification des conditions d'utilisation
+      </h3>
+      <p class="mb-4">
+        5.1. La Famiglia SAS se réserve le droit de modifier ces conditions
+        d'utilisation à tout moment. Les utilisateurs seront informés des
+        changements par le biais du site.
+      </p>
+
+      <p class="mt-8">
+        En utilisant ce site et nos services, vous acceptez pleinement ces
+        conditions d'utilisation. Si vous n'êtes pas d'accord avec l'une de ces
+        clauses, veuillez ne pas utiliser nos services de réservation en ligne.
+      </p>
+      <p class="mt-8">
+        Pour toute question ou préoccupation, veuillez nous contacter à
+        <a href="mailto:lafamiglia-contact@gmail.com" class="text-blue-600">
+          lafamiglia-contact@gmail.com</a
+        >
+      </p>
+    </div>
+
+    <Footer />
+  </div>
 </template>
