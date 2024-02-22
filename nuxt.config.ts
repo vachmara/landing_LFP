4a1a1cf6beabeece2e71be258b0d91a409366d81
Valentin Chmara <valentinchmara@gmail.com>
2024-02-22 11:29:02 +0100

    feat: add gtm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxtjs/robots", "nuxt-simple-sitemap"],
+  modules: [
+    "@nuxt/ui",
+    "@nuxtjs/robots",
+    "nuxt-simple-sitemap",
+    "@zadigetvoltaire/nuxt-gtm",
+  ],
   css: ["~/assets/style/main.scss"],
   colorMode: {
     preference: "light",
+  },
+  gtm: {
+    id: "GTM-PRC9ZM7W",
+    compatibility: false,
   },
 });
